2641ac5bd9f8fadfbd2594760f5e2bac74805da7
Mirian Dzhachvadze <mirian.dzhachvadze@gmail.com>
2018-12-03 22:07:28 +0300

    add git4idea plugin

--- src/main/kotlin/app/kt/PurgeLocalBranchesAction.kt
+++ src/main/kotlin/app/kt/PurgeLocalBranchesAction.kt
@@ -3,6 +3,7 @@ package app.kt
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.DialogBuilder
+import git4idea.GitUtil
 import javax.swing.JComponent
 
 
@@ -11,6 +12,9 @@ class PurgeLocalBranchesAction: AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
         val activeProjects = ProjectUtil.listActiveProjects(e.project)
         println("activeProjects = $activeProjects")
+
+//        val repositoryManager = GitUtil.getRepositoryManager(e.project!!)
+//        repositoryManager.
 
         val builder = DialogBuilder(e.project)
         builder.setDimensionServiceKey("GrepConsoleTailFileDialog")

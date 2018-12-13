f1953f60a40abdcb9daa26754a1f831a007f6bd4
Mirian Dzhachvadze <mirian.dzhachvadze@gmail.com>
2018-12-13 22:36:38 +0300

    add headers

--- src/main/kotlin/app/gui/PurgeLocalBranchesDialog.kt
+++ src/main/kotlin/app/gui/PurgeLocalBranchesDialog.kt
@@ -4,6 +4,8 @@ import app.ProjectUtil
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
+import com.intellij.util.ui.JBUI
+import git4idea.GitLocalBranch
 import git4idea.GitRemoteBranch
 import git4idea.repo.GitRepository
 import javax.swing.JComboBox
@@ -22,10 +24,13 @@ class PurgeLocalBranchesDialog(project: Project?) : DialogWrapper(project) {
     private lateinit var branchesTable: JTable
     private lateinit var projectsComboBox: JComboBox<ProjectInfo>
 
+    private val branchesTableModel = BranchesTableModel()
+
     init {
         title = "Purge local branches"
         setOKButtonText("Purge")
 
+        initBranchesTable()
         initProjectComboBox(project)
         init()
     }
@@ -35,27 +40,39 @@ class PurgeLocalBranchesDialog(project: Project?) : DialogWrapper(project) {
         val projectInfos = repositories.map { ProjectInfo(it) }
         projectInfos.forEach { projectsComboBox.addItem(it) }
 
-        updateBranchesTable(projectInfos[0])
+        branchesTableModel.updateBranches(projectInfos[0])
 
         projectsComboBox.addActionListener { event ->
             val selectedProject = (event.source as JComboBox<*>).selectedItem
-            updateBranchesTable(selectedProject as ProjectInfo)
+            branchesTableModel.updateBranches(selectedProject as ProjectInfo)
         }
     }
 
-    private fun updateBranchesTable(projectInfo: ProjectInfo) {
-        branchesTable.model = BranchesTableModel(projectInfo)
-        branchesTable.columnModel.getColumn(0).maxWidth = 30
-        branchesTable.columnModel.getColumn(2).maxWidth = 30
+    private fun initBranchesTable() {
+        branchesTable.model = branchesTableModel
+        branchesTable.rowHeight = JBUI.scale(22)
+
+        val columnModel = branchesTable.columnModel
+        val selectColumn = columnModel.getColumn(0)
+        selectColumn.headerValue = "Select"
+        selectColumn.maxWidth = 50
+
+        val branchColumn = columnModel.getColumn(1)
+        branchColumn.headerValue = "Branch"
+
+        val existOnRemoteColumn = columnModel.getColumn(2)
+        existOnRemoteColumn.headerValue = "Exist on remote"
+        existOnRemoteColumn.maxWidth = 150
     }
 
     override fun createCenterPanel(): JComponent? {
         return mainPanel
     }
-
 }
 
-class BranchesTableModel(private val projectInfo: ProjectInfo) : DefaultTableModel() {
+class BranchesTableModel: DefaultTableModel() {
+
+    var removeCandidates = mutableListOf<GitLocalBranch>()
 
     companion object {
         // like private static final in java
@@ -65,21 +82,41 @@ class BranchesTableModel(private val projectInfo: ProjectInfo) : DefaultTableMod
 
     init {
         COLUMN_NAME.forEach { addColumn(it) }
+    }
 
-        val localBranches = projectInfo.gitRepo.branches.localBranches
+    override fun isCellEditable(row: Int, column: Int): Boolean = column == 0
+
+    override fun getColumnClass(columnIndex: Int): Class<*> = COLUMN_CLASS[columnIndex]
+
+    override fun getColumnCount(): Int  = 3
+
+    fun updateBranches(projectInfo: ProjectInfo) {
+        clearTable()
+
+        val localBranches: MutableCollection<GitLocalBranch> = projectInfo.gitRepo.branches.localBranches
         val remoteBranches: MutableCollection<GitRemoteBranch> = projectInfo.gitRepo.branches.remoteBranches
         localBranches.forEach { localBranch ->
             val isLocalBranchExistsOnRemote = remoteBranches.any {
                 remoteBranch -> remoteBranch.nameForRemoteOperations == localBranch.name
             }
 
+            if (!isLocalBranchExistsOnRemote) {
+                removeCandidates.add(localBranch)
+            }
+
             addRow(arrayOf(!isLocalBranchExistsOnRemote, localBranch.name, isLocalBranchExistsOnRemote))
         }
     }
 
-    override fun isCellEditable(row: Int, column: Int): Boolean = column == 0
+    private fun clearTable() {
+        if (rowCount > 0) {
+            for (i in 0..rowCount) {
+                removeRow(i)
+            }
+        }
 
-    override fun getColumnClass(columnIndex: Int): Class<*> = COLUMN_CLASS[columnIndex]
+        removeCandidates.clear()
+    }
 }
 
 data class ProjectInfo(val gitRepo: GitRepository) {
